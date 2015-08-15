63fadddaa2923f31cf983fa2f606dd3c900780af
Prashant Kumar <pkumar0508@gmail.com>
2015-08-15 11:54:31 -0400

    Update assignment3.js

--- webgl/assignment3.js
+++ webgl/assignment3.js
@@ -71,55 +71,39 @@ window.onload = function init()
     render();
 }
 
+var vertices = [
+    vec4( -0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5, -0.5, -0.5, 1.0 )
+];
+
 function colorCube()
 {
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+    //quad( 1, 0, 3, 2 );
+    //quad( 2, 3, 7, 6 );
+    //quad( 3, 0, 4, 7 );
+    //quad( 6, 5, 1, 2 );
+    //quad( 4, 5, 6, 7 );
+    //quad( 5, 4, 0, 1 );
+    var indices = [0, 1, 2];
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        colors.push( [ 0.0, 0.0, 0.0, 1.0 ] );  // black
+    }
 }
 
 function quad(a, b, c, d)
 {
-    var vertices = [
-        vec4( -0.5, -0.5,  0.5, 1.0 ),
-        vec4( -0.5,  0.5,  0.5, 1.0 ),
-        vec4(  0.5,  0.5,  0.5, 1.0 ),
-        vec4(  0.5, -0.5,  0.5, 1.0 ),
-        vec4( -0.5, -0.5, -0.5, 1.0 ),
-        vec4( -0.5,  0.5, -0.5, 1.0 ),
-        vec4(  0.5,  0.5, -0.5, 1.0 ),
-        vec4(  0.5, -0.5, -0.5, 1.0 )
-    ];
-
-    var vertexColors = [
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 0.0, 0.0, 0.0, 1.0 ]   // black
-    ];
-
-    // We need to parition the quad into two triangles in order for
-    // WebGL to be able to render it.  In this case, we create two
-    // triangles from the quad indices
-
-    //vertex color assigned by the index of the vertex
-
     var indices = [ a, b, c, a, c, d ];
 
     for ( var i = 0; i < indices.length; ++i ) {
         points.push( vertices[indices[i]] );
-        colors.push( vertexColors[indices[i]] );
-
-        // for solid colored faces use
-        //colors.push(vertexColors[a]);
-
+        colors.push( [ 0.0, 0.0, 0.0, 1.0 ] );  // black
     }
 }
 
