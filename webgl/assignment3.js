5c221a78d357fe2a537fe1bdc0c887c6b5f02320
Prashant Kumar <pkumar0508@gmail.com>
2015-08-15 12:07:59 -0400

    Update assignment3.js

--- webgl/assignment3.js
+++ webgl/assignment3.js
@@ -84,28 +84,19 @@ var vertices = [
 
 function colorCube()
 {
-    //quad( 1, 0, 3, 2 );
-    //quad( 2, 3, 7, 6 );
-    //quad( 3, 0, 4, 7 );
-    //quad( 6, 5, 1, 2 );
-    //quad( 4, 5, 6, 7 );
-    //quad( 5, 4, 0, 1 );
-    var indices = [0, 1, 2, 3, 7, 6, 5, 4];
+    var indices =
+    [ 1, 0, 3, 2 ,
+    2, 3, 7, 6 ,
+    3, 0, 4, 7 ,
+    6, 5, 1, 2 ,
+    4, 5, 6, 7 ,
+    5, 4, 0, 1];
+    
     for ( var i = 0; i < indices.length; ++i ) {
         points.push( vertices[indices[i]] );
         colors.push( [ 0.0, 0.0, 0.0, 1.0 ] );  // black
     }
     NumVertices = indices.length;
-}
-
-function quad(a, b, c, d)
-{
-    var indices = [ a, b, c, a, c, d ];
-
-    for ( var i = 0; i < indices.length; ++i ) {
-        points.push( vertices[indices[i]] );
-        colors.push( [ 0.0, 0.0, 0.0, 1.0 ] );  // black
-    }
 }
 
 function render()
@@ -115,7 +106,7 @@ function render()
     theta[axis] += 2.0;
     gl.uniform3fv(thetaLoc, theta);
 
-    gl.drawArrays( gl.LINE_STRIP, 0, NumVertices );
+    gl.drawArrays( gl.QUADS, 0, NumVertices );
 
     requestAnimFrame( render );
 }
