fc60bef41669d04dc7a51109c51164ed6396e82c
Prashant Kumar <pkumar0508@gmail.com>
2015-08-16 00:03:04 -0400

    Update assignment3.js

--- webgl/assignment3.js
+++ webgl/assignment3.js
@@ -3,6 +3,8 @@
 var canvas;
 var gl;
 
+var FULL_CIRCLE = 2 * Math.PI;
+
 var NumVertices;
 
 var points = [];
@@ -19,7 +21,7 @@ var thetaLoc;
 
 window.onload = function init()
 {
-    document.getElementById("abc").innerHTML = 'Version 2.0';
+    document.getElementById("abc").innerHTML = 'Version 2.1';
     canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
@@ -88,6 +90,24 @@ function drawLineStrip(lines) {
     });
 
     NumVertices += indices.length;
+}
+
+function sphere(r) {
+    // parametric equations for sphere
+    // x = rho * sin(phi) * cos(theta)
+    // y = rho * sin(phi) * sin(theta)
+    // z = rho * cos(phi)
+    var theta = 0;
+    lines = [];
+    for (var phi = 0; phi <= FULL_CIRCLE; phi += 0.05 * FULL_CIRCLE) {
+        lines.push(vec4(
+            r * Math.sin(phi) * Math.cos(theta),
+            r * Math.sin(phi) * Math.sin(theta),
+            r * Math.cos(phi),
+            1.0
+        ));
+    }
+    drawLineStrip(lines);
 }
 
 function cube(x, y, z)
